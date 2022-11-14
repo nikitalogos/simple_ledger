bf3b592608454fb22a7432a6c133856712d283b9
Nikita Logos <thelogosnik@gmail.com>
2022-11-14 11:25:38 +0200

    Add improve transaction format 2

--- quasar_app/src/components/ParseVisualizer.vue
+++ quasar_app/src/components/ParseVisualizer.vue
@@ -25,6 +25,53 @@ export default defineComponent({
       return value_str
     },
   },
+  computed: {
+    transactions_formatted() {
+      return this.settings.parse_tree.map((transaction) => {
+        const date_expr = transaction.date_expr
+        const account_expr = transaction.account_expr
+        const amount_expr = transaction.amount_expr
+
+        let date = 'date?'
+        if (date_expr) {
+          const mdate = moment(date_expr.date)
+          const date_abs = mdate.format('ddd, YYYY MMM Do')
+          let date_rel = mdate.calendar(null, {
+              sameDay: '[Today]',
+              nextDay: '[Tomorrow]',
+              lastDay: '[Yesterday]',
+              nextWeek: '[]',
+              lastWeek: '[]',
+              sameElse: '[]'
+          })
+          date_rel = date_rel || mdate.from(moment().startOf('day'))
+
+          date = `${date_abs} (${date_rel})`
+        }
+
+        const result = {
+          date: {
+            is_valid: !!date_expr,
+            text: date,
+          },
+          acc_from: {
+            is_valid: !!account_expr,
+            text: account_expr ? account_expr.acc_from : 'from?',
+          },
+          acc_to: {
+            is_valid: !!account_expr,
+            text: account_expr ? account_expr.acc_to : 'from?',
+          },
+          amount: {
+            is_valid: !!amount_expr,
+            text: amount_expr ? `${this.format_float(amount_expr.amount)} ${amount_expr.currency}` : 'amount?',
+          },
+        }
+        result.is_visible = Object.keys(result).some((key) => result[key].is_valid)
+        return result
+      })
+    }
+  },
   created() {
     const { settings } = useSettings()
     this.settings = settings
@@ -34,12 +81,31 @@ export default defineComponent({
 
 <template>
   <div class="tr-list">
-    <div class="tr-item"  v-for="(transaction, idx) in settings.parse_tree" :key="idx">
-      <div class="tr-part date label">{{ moment(transaction.date_expr.date).format('ddd, YYYY MMM Do') }}</div>
-      <div class="tr-part account label">{{ transaction.account_expr.acc_from }}</div>
-      <div class="tr-part">{{ "➞" }}</div>
-      <div class="tr-part account label">{{ transaction.account_expr.acc_to }}</div>
-      <div class="tr-part amount label">{{ `${format_float(transaction.amount_expr.amount)} ${transaction.amount_expr.currency}` }}</div>
+    <div v-for="(transaction, idx) in transactions_formatted" :key="idx">
+      <div v-if="transaction.is_visible" class="tr-item">
+        <div
+          class="tr-part date label"
+          :class="{disabled: !transaction.date.is_valid}"
+        >{{ transaction.date.text }}</div>
+        <div
+          class="tr-part account label"
+          :class="{disabled: !transaction.acc_from.is_valid}"
+        >{{ transaction.acc_from.text }}</div>
+        <div
+          class="tr-part"
+        >{{ "➞" }}</div>
+        <div
+          class="tr-part account label"
+          :class="{disabled: !transaction.acc_to.is_valid}"
+        >{{ transaction.acc_to.text }}</div>
+        <div
+          class="tr-part amount label"
+          :class="{disabled: !transaction.amount.is_valid}"
+        >{{ transaction.amount.text }}</div>
+      </div>
+      <div v-else class="tr-item">
+        <pre> </pre>
+      </div>
     </div>
   </div>
 </template>
@@ -51,11 +117,17 @@ export default defineComponent({
   display: flex;
   flex-direction: column;
   width: 50%;
-  padding-left: 20px;
+  padding-left: 10px;
+
+  overflow-x: scroll;
 
   .tr-item {
     display: flex;
     flex-direction: row;
+    height: 1.5rem;
+    padding: 2px;
+
+    white-space: nowrap;
 
     .tr-part {
       margin-right: 10px;
