148cc1507a528294c54e29c0b5a34819a6e831b8
Nikita Logos <thelogosnik@gmail.com>
2022-11-25 02:34:42 +0200

    Add scatterplot

--- quasar_app/src/components/graph/GraphCanvas.vue
+++ quasar_app/src/components/graph/GraphCanvas.vue
@@ -14,6 +14,8 @@ export default defineComponent({
     return {
       editor: null,
       tl: null,
+
+      selected_account: 'card_rub'
     }
   },
   computed: {
@@ -38,6 +40,58 @@ export default defineComponent({
     now_text() {
       return (this.now_line_x < 0 ? '< ' : '') + 'now' + (this.now_line_x < this.el_width ? '' : ' >')
     },
+
+    points() {
+      if (!this.editor?.transactions){
+        return null
+      }
+
+      const points = []
+      let sum = 0
+      this.editor.transactions.forEach((transaction) => {
+        let amount
+        if (transaction.acc_to === this.selected_account) {
+          amount = transaction.amount
+        } else if (transaction.acc_from === this.selected_account) {
+          amount = -transaction.amount
+        } else {
+          return
+        }
+        sum += amount
+        const time = transaction.mdate.valueOf()
+        points.push({
+          amount,
+          time,
+          sum,
+        })
+      })
+      points.sort((a, b) => a.time - b.time)
+
+      const amounts = points.map((point) => point.sum)
+      const amount_min = Math.min(...amounts)
+      const amount_max = Math.max(...amounts)
+      const amount_spread = amount_max - amount_min
+      const padding_px = 10
+
+      let x_last = 0
+      let y_last = 0
+
+      points.forEach((point) => {
+        const amount_normed = (point.sum - amount_min) / amount_spread
+        const x = (point.time - this.tl.start_time) / this.tl.duration * this.el_width
+        const y = (1 - amount_normed) * (this.el_height - padding_px * 2) + padding_px
+        Object.assign(point, {
+          x: x,
+          y: y,
+          dx: x - x_last,
+          dy: y - y_last,
+        })
+        x_last = x
+        y_last = y
+      })
+
+      return points
+    },
   },
   methods: {
     fit_now() {
@@ -58,6 +112,36 @@ export default defineComponent({
   <div :id="id" class="graph-canvas">
     <div class="now-line" :style="{left: now_line_x + 'px'}"></div>
     <div class="now-text" :style="{left: now_text_x + 'px'}" @dblclick="fit_now()">{{ now_text }}</div>
+    <div
+      v-for="(point, idx) in points"
+      :key="'point' + idx"
+      class="point"
+      :style="{left: point.x + 'px', top: point.y + 'px'}"
+    >
+    </div>
+    <div
+      v-for="(point, idx) in points.slice(1)"
+      :key="'hline' + idx"
+      class="line"
+      :style="{
+        left: (point.x - point.dx) + 'px',
+        top: (point.y - point.dy) + 'px',
+        height: '1px',
+        width: point.dx + 'px',
+      }"
+    ></div>
+    <div
+      v-for="(point, idx) in points.slice(1)"
+      :key="'vline' + idx"
+      class="line"
+      :style="{
+        left: point.x + 'px',
+        top: (point.dy > 0 ? (point.y - point.dy) : point.y) + 'px',
+        height: Math.abs(point.dy) + 'px',
+        width: '1px',
+      }"
+    ></div>
+
   </div>
 </template>
 
@@ -76,15 +160,30 @@ export default defineComponent({
     z-index: 100;
     height: 100%;
     width: 1px;
-    top: 0;
     background-color: red;
   }
   .now-text {
     position: absolute;
     z-index: 100;
-    top: 0;
     color: red;
     user-select: none;
+  }
+
+  .point {
+    position: absolute;
+    z-index: 100;
+    color: black;
+
+    width: 10px;
+    height: 10px;
+    border-radius: 5px;
+    margin: -5px;
+    background-color: red;
+  }
+  .line {
+    position: absolute;
+    z-index: 100;
+    background-color: red;
   }
 }
 </style>
--- quasar_app/src/composables/editor.ts
+++ quasar_app/src/composables/editor.ts
@@ -7,27 +7,10 @@ const parse = (string) => text_parser.parse(string, { actions, types })
 
 const editor = reactive({
   text: `
-# Planning
-
-10.16
-    - >cash 2800 ils
-    - >cibus 0 ils
-
-?? 17% tax in the hotel - 3355 x 60 x 0.17 = 34k = 1500 ils
-
-10.31 salary> 13k ils
-11.03 premia > 275k rub
-11.03 salary > 49k rub
-11.03 > oleg_solodovnik 334.5k rub
-
----
-
-11.15 salary > 4.8k ils
-
-11.15 ticket_chargeback > 60k rub (3278 ils)
-
-11.25 >cibus 1300 ils
-
+11.25 salary> 100k
+11.26 salary> 10k
+11.27 >food 30k
+11.28 >beauty 50k
 `
 })
 editor.parse_tree = computed(() => {
@@ -37,12 +20,15 @@ editor.parse_tree = computed(() => {
 })
 editor.transactions = computed(() => {
   const transactions = []
-  this.editor.parse_tree.forEach((item) => {
+  editor.parse_tree.forEach((item) => {
     const date_expr = item.date_expr
     const account_expr = item.account_expr
     const amount_expr = item.amount_expr
 
     if (!date_expr || date_expr.error || !account_expr || !amount_expr) {
+      return
+    }
+    if (account_expr.acc_from === account_expr.acc_to) {
       return
     }
     transactions.push({
